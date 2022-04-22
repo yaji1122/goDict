f63973b13def69f976806d87b7d161ec325667cd
yaji <yajilin@fontrip.com>
2022-04-22 16:34:28 +0800

    add Jenkinsfile

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ func main() {
 	r := setupRouter()
 
 	// Listen and Server in 0.0.0.0:8080
-	err := r.Run(":8080")
+	err := r.Run(":8081")
 	if err != nil {
 		log.Panic(err)
 	}
--- scripts/entrypoint.dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-GO111MODULE=off go get github.com/githubnemo/CompileDaemon
-
-CompileDaemon --build="go build -o ./../goDict ./../cmd/*.go" --command=./../goDict
